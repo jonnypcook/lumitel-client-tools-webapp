ecb6258a1584b249e21cabd7468ad6ff1a12ba99
jonnypcook <jonny.p.cook@8point3led.co.uk>
2017-04-13 12:22:11 +0100

    Added simple layout, pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,54 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {FormsModule}   from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+import {AppComponent} from './app.component';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import {NAV_DROPDOWN_DIRECTIVES} from './shared/nav-dropdown.directive';
 
-import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
-import { AsideToggleDirective } from './shared/aside.directive';
-import { BreadcrumbsComponent } from './shared/breadcrumb.component';
+import {ChartsModule} from 'ng2-charts/ng2-charts';
+import {SIDEBAR_TOGGLE_DIRECTIVES} from './shared/sidebar.directive';
+import {AsideToggleDirective} from './shared/aside.directive';
+import {BreadcrumbsComponent} from './shared/breadcrumb.component';
+
+// http module
+import {HttpModule} from '@angular/http';
 
 // Routing Module
-import { AppRoutingModule } from './app.routing';
+import {AppRoutingModule} from './app.routing';
 
 //Layouts
-import { FullLayoutComponent } from './layouts/full-layout.component';
+import {FullLayoutComponent} from './layouts/full-layout.component';
+import {SimpleLayoutComponent} from './layouts/simple-layout.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BsDropdownModule.forRoot(),
-    TabsModule.forRoot(),
-    ChartsModule
-  ],
-  declarations: [
-    AppComponent,
-    FullLayoutComponent,
-    NAV_DROPDOWN_DIRECTIVES,
-    BreadcrumbsComponent,
-    SIDEBAR_TOGGLE_DIRECTIVES,
-    AsideToggleDirective
-  ],
-  providers: [{
-    provide: LocationStrategy,
-    useClass: HashLocationStrategy
-  }],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        BsDropdownModule.forRoot(),
+        TooltipModule.forRoot(),
+        TabsModule.forRoot(),
+        ChartsModule,
+        HttpModule,
+    ],
+    declarations: [
+        AppComponent,
+        FullLayoutComponent,
+        SimpleLayoutComponent,
+        NAV_DROPDOWN_DIRECTIVES,
+        BreadcrumbsComponent,
+        SIDEBAR_TOGGLE_DIRECTIVES,
+        AsideToggleDirective
+    ],
+    providers: [{
+        provide: LocationStrategy,
+        useClass: HashLocationStrategy
+    }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,32 +1,47 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 //Layouts
-import { FullLayoutComponent } from './layouts/full-layout.component';
+import {FullLayoutComponent} from './layouts/full-layout.component';
+import {SimpleLayoutComponent}    from './layouts/simple-layout.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  },
-  {
-    path: '',
-    component: FullLayoutComponent,
-    data: {
-      title: 'Home'
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
     },
-    children: [
-      {
-        path: 'dashboard',
-        loadChildren: './dashboard/dashboard.module#DashboardModule'
-      },
-    ]
-  }
+    {
+        path: '',
+        component: FullLayoutComponent,
+        data: {
+            title: 'Home'
+        },
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: './dashboard/dashboard.module#DashboardModule'
+            },
+        ]
+    },
+    {
+        path: '',
+        component: SimpleLayoutComponent,
+        data: {
+            title: 'Pages'
+        },
+        children: [
+            {
+                path: '',
+                loadChildren: './pages/pages.module#PagesModule',
+            }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
